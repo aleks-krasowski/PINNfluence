b94c007c5fb0760813b5f5647a5596ba7162aa10
Aleksander Krasowski <aleksander.krasowski@hhi.fraunhofer.de>
2025-01-27 16:08:25 +0100

    add MPS fallback + formatting

--- _utils/utils.py
+++ _utils/utils.py
@@ -1,5 +1,7 @@
-import deepxde as dde 
-import torch 
+import deepxde as dde
+import os
+import torch
+
 
 class StopOnBrokenLBFGS(dde.callbacks.Callback):
     """
@@ -20,30 +22,34 @@ class StopOnBrokenLBFGS(dde.callbacks.Callback):
         n_iter = self.model.opt.state_dict()["state"][0]["n_iter"]
 
         # if only one iteration was executed although more are defined we assume that something broke
-        if (n_iter - self.prev_n_iter == 1) and (dde.optimizers.LBFGS_options["iter_per_step"] != 1):
+        if (n_iter - self.prev_n_iter == 1) and (
+            dde.optimizers.LBFGS_options["iter_per_step"] != 1
+        ):
             self.model.stop_training = True
             print("Encountered broken LBFGS. Stopping training.")
 
         self.prev_n_iter = n_iter
 
-def set_default_device(device: str = 'cpu'): 
-    if device == 'cpu':
-        torch.set_default_device('cpu')
+
+def set_default_device(device: str = "cpu"):
+    if device == "cpu":
+        torch.set_default_device("cpu")
         print("Using CPU")
-    elif device == 'cuda':
+    elif device == "cuda":
         if torch.cuda.is_available():
-            torch.set_default_device('cuda')
+            torch.set_default_device("cuda")
             print("Using CUDA")
         else:
             print("CUDA not available. Using CPU")
-            torch.set_default_device('cpu')
-    elif device == 'mps':
+            torch.set_default_device("cpu")
+    elif device == "mps":
         if torch.backends.mps.is_built() and torch.backends.mps.is_available():
-            torch.set_default_device('mps')
+            torch.set_default_device("mps")
+            os.environ["PYTORCH_ENABLE_MPS_FALLBACK"] = "1"
             print("Using MPS")
         else:
             print("MPS not available. Using CPU")
-            torch.set_default_device('cpu')
+            torch.set_default_device("cpu")
     else:
         print("Invalid device. Using CPU")
-        torch.set_default_device('cpu')
+        torch.set_default_device("cpu")
