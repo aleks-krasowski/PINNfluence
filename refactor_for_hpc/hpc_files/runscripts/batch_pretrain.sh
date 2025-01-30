faf043beb490e9fa43218f198d9d1ecd8249ca46
Aleksander Krasowski <aleksander.krasowski@hhi.fraunhofer.de>
2025-01-31 00:32:27 +0100

    add hpc files

---
+++ refactor_for_hpc/hpc_files/runscripts/batch_pretrain.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=burgers_pretrain
+#SBATCH --mail-type=ALL
+
+#         Output (stdout and stderr) of this job will go into a file named with SLURM_JOB_ID (%j) and the job_name (%x)
+#SBATCH --output=./runs/%j_%x.out
+
+#SBATCH --partition=amd1,amd2
+
+#         Tell slurm to run the job on a single node. Always use this statement.
+#SBATCH --nodes=1
+
+#         Ask slurm to run at most 1 task (slurm task == OS process). A task (process) might launch subprocesses/threads.
+#SBATCH --ntasks=1
+
+#         Max number of cpus per process (threads/subprocesses) is 16. Seems reasonable with 4 GPUs on a 64 core machine.
+#SBATCH --cpus-per-task=6
+
+#         Request RAM for your job
+#SBATCH --mem=4G
+
+#####################################################################################
+
+# This included file contains the definition for $LOCAL_JOB_DIR to be used locally on the node.
+source "/etc/slurm/local_job_dir.sh"
+
+# Launch the apptainer image with --nv for nvidia support. Two bind mounts are used: 
+# - One for the ImageNet dataset and 
+# - One for the results (e.g. checkpoint data that you may store in $LOCAL_JOB_DIR on the node
+apptainer run --nv --bind ${LOCAL_JOB_DIR}:/opt/model_zoo --bind ${HOME}/pinnfluence_finetuning/pinnfluence:/opt/code ./deepxde_generic_code.sif  /opt/code/burgers/pretrain_model.py 
+
+
+# This command copies all results generated in $LOCAL_JOB_DIR back to the submit folder regarding the job id.
+cp -r ${LOCAL_JOB_DIR} ${SLURM_SUBMIT_DIR}/${SLURM_JOB_ID}
