9835ece47096d3085599c464eb47ef3baa09f3b2
Aleksander Krasowski <aleksander.krasowski@hhi.fraunhofer.de>
2025-01-30 16:12:08 +0100

    add trash bin lol

---
+++ experiments/burgers_equation/_experimental/finetune_random_trashy.py
@@ -0,0 +1,30 @@
+import deepxde as dde
+import numpy as np
+import torch
+from train import burgers_equation, ic, bc, geomtime
+from defaults import DEFAULTS
+
+from _utils.utils import sample_new_training_points_via_IF, get_checkpoint_file
+
+net = dde.nn.FNN(DEFAULTS["layers"], "tanh", "Glorot normal")
+
+pde = burgers_equation
+dde.config.set_random_seed(42)
+data = dde.data.TimePDE(
+    geomtime,
+    pde,
+    ic_bcs=[bc, ic],
+    num_domain=DEFAULTS["num_domain"],
+    num_boundary=DEFAULTS["num_boundary"],
+    num_initial=DEFAULTS["num_initial"],
+    num_test=DEFAULTS["num_domain"],
+)
+
+checkpoint_file = get_checkpoint_file(DEFAULTS["save_path"], "adam")
+net.load_state_dict(torch.load(checkpoint_file)["model_state_dict"])
+data.resample_train_points()
+
+model = dde.Model(data, net)
+model.compile("adam", lr=DEFAULTS["lr"] * 0.1)
+model.train(iterations=50_000, display_every=1000)
+model.save(f"{DEFAULTS['save_path']}/finetuned_random")
